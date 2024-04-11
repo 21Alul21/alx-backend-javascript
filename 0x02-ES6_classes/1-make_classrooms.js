711fb1e3e914d779116c271b2615e827c5b99e2d
21Alul21 <augustine.alul@yahoo.com>
2024-04-11 19:42:21 +0000

    added second task solution

---
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -0,0 +1,11 @@
+import Classroom from './0-classroom';
+
+function initializeRooms() {
+  const obj1 = new Classroom(19);
+  const obj2 = new Classroom(20);
+  const obj3 = new Classroom(31);
+  const array = [obj1, obj2, obj3];
+  return array;
+}
+
+export default initializeRooms;
