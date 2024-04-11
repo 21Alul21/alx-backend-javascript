aa5441e8c3c8832c835b646a5aab8388acf23c06
21Alul21 <augustine.alul@yahoo.com>
2024-04-11 20:33:50 +0000

    solution to task 3

---
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,54 @@
+class HolbertonCourse {
+  constructor(name, length, students) {
+    if (typeof name !== 'string') {
+      throw new TypeError('name must be a string');
+    }
+    if (typeof length !== 'number') {
+      throw new TypeError('length must be a number');
+    }
+    if (!Array.isArray(students) || students.some((student) => typeof student !== 'string')) {
+      throw new TypeError('students must be an array of strings');
+    }
+    this._name = name;
+    this._length = length;
+    this._students = students;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(value) {
+    if (typeof value === 'string') {
+      this._name = value;
+    } else {
+      throw new TypeError('name must be a string');
+    }
+  }
+
+  get length() {
+    return this._length;
+  }
+
+  set length(value) {
+    if (typeof value === 'number') {
+      this._length = value;
+    } else {
+      throw new TypeError('length must be a number');
+    }
+  }
+
+  get students() {
+    return this._students;
+  }
+
+  set students(value) {
+    if (Array.isArray(value) && value.every((student) => typeof student === 'string')) {
+      this._students = value;
+    } else {
+      throw new TypeError('students must be an array of strings');
+    }
+  }
+}
+
+export default HolbertonCourse;
