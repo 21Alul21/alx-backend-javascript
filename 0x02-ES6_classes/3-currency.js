fe22514140feb403e566098e184a2047e13be26d
21Alul21 <augustine.alul@yahoo.com>
2024-04-12 05:04:37 +0000

    update to number 3

--- 0x02-ES6_classes/3-currency.js
+++ 0x02-ES6_classes/3-currency.js
@@ -1,33 +1,32 @@
 class Currency {
-    constructor(code, name) {
-        this._code = String(code);
-        this._name = String(name);
-    }
+  constructor(code, name) {
+    this._code = String(code);
+    this._name = String(name);
+  }
 
-    get code() {
-        return this._code;
-    }
+  get code() {
+    return this._code;
+  }
 
-    set code(value) {
-        if (typeof value === 'string') {
-            this._code = value;
-        }
+  set code(value) {
+    if (typeof value === 'string') {
+      this._code = value;
     }
+  }
 
-    get name() {
-        return this._name;
-    }
+  get name() {
+    return this._name;
+  }
 
-    set name(value) {
-        if (typeof value === 'string') {
-            this._name = value;
-        }
+  set name(value) {
+    if (typeof value === 'string') {
+      this._name = value;
     }
+  }
 
-    displayFullCurrency() {
-        return `${this.name} (${this.code})`;
-    }
+  displayFullCurrency() {
+    return `${this.name} (${this.code})`;
+  }
 }
 
 export default Currency;
-
