a9dfee1dc44eb09be503788f59286d30a33e58dd
21Alul21 <augustine.alul@yahoo.com>
2024-04-14 00:34:10 +0000

    solution to task 10 added

--- 0x02-ES6_classes/10-car.js
+++ 0x02-ES6_classes/10-car.js
@@ -1,11 +1,17 @@
+const brandSymbol = Symbol('brand');
+const motorSymbol = Symbol('motor');
+const colorSymbol = Symbol('color');
+
 class Car {
   constructor(brand, motor, color) {
-    this._brand = brand;
-    this._motor = motor;
-    this._color = color;
+    this[brandSymbol] = brand;
+    this[motorSymbol] = motor;
+    this[colorSymbol] = color;
   }
 
   cloneCar() {
-    return new Car(this._brand, this._motor, this._color);
+    return new Car(this[brandSymbol], this[motorSymbol], this[colorSymbol]);
   }
 }
+
+export default Car;
